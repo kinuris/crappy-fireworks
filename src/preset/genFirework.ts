8e3be89f4bd9ddaad698ef811fca9bc3892faab0
CDPC <chanchrisdominic@gmail.com>
2021-09-13 12:09:58 +0800

    randomized lifetime per firework band

--- src/preset/genFirework.ts
+++ src/preset/genFirework.ts
@@ -12,20 +12,22 @@ export class Firework {
             const count = Math.random() * 20 + 20
             const color = Color.genRandColor(new Color(255, 255, 255, 1))
             const intensity = Math.random() * 1800
-
+            const lifetime = Math.random() * 8 + 8
+            
             for(let i = 0; i < count; i++) {
                 let randomAngle = Math.random() * Math.PI * 2
-                let x = (Math.cos(randomAngle)) * intensity + position.x
-                let y = (Math.sin(randomAngle)) * intensity + position.y
-
-                let randPoint = new Point(x, y)
-                let { x: xComponent, y: yComponent } = position.component(randPoint)
-                let distance = randPoint.distance(position)
-    
+                let x = Math.cos(randomAngle) * intensity + position.x
+                let y = Math.sin(randomAngle) * intensity + position.y
+                
                 let radius = Math.random() * 10 + 2
+                let randPoint = new Point(x, y)
+                let distance = randPoint.distance(position)
+
+                let { x: xComponent, y: yComponent } = position.component(randPoint)
+    
                 let velocity = new Point(xComponent * (distance/250), yComponent * (distance/250))
 
-                output.push(new Circle(radius, position).setSteps(1).setLifetime(10).setRadiusThreshold(0.2).setVelocity(velocity).setColor(color))
+                output.push(new Circle(radius, position).setSteps(1).setLifetime(lifetime).setRadiusThreshold(0.2).setVelocity(velocity).setColor(color))
             } 
         }
 
